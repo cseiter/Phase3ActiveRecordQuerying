dc1c19da8d852fc6f25e80c29fd68fefa9aff784
chris <cseiter@gmail.com>
2021-07-31 15:23:08 -0400

    least tests pass

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,15 @@ class Show < ActiveRecord::Base
     def self.most_popular_show
         self.where("rating=?",self.maximum(:rating)).first
     end
+
+    def self.lowest_rating
+        self.minimum(:rating)
+    end
+
+    def self.least_popular_show
+        self.where("rating=?",self.minimum(:rating)).first
+    end
+
+
+
 end
